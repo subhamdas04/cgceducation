67dc5f004c7269bdb22038a6daad131d8c39020f
subhamdas04 <subhamdas0465@gmail.com>
2022-12-01 23:31:43 +0530

    d

---
+++ about-us/index.php
@@ -0,0 +1,47 @@
+<?php 
+include_once('http://localhost/ghotalanews/header/header.php');
+ ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" href="../header/style.css">
+    <link rel="stylesheet" href="../footer/style.css">
+    <link rel="stylesheet" href="about-us-style.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Us</title>
+</head>
+
+<body>
+    <center>
+        <h1>ABOUT US</h1>
+        <div class="about">
+            <img src="../img/apple.jpg" alt="ABOUT US PIC" width="500px">
+            <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores consectetur aliquam architecto suscipit
+                rem. Temporibus nobis delectus doloribus alias magni! Vero sequi odit veritatis consequuntur. Accusamus,
+                quisquam. Facere ab quo voluptate omnis voluptatibus dicta minus nostrum quia officia eius, odio dolores
+                cum minima porro quos sunt quod enim laboriosam? Quidem illum tempora distinctio voluptas. Facere id
+                eligendi reiciendis cupiditate omnis deleniti dicta, obcaecati architecto! A facilis aspernatur
+                voluptates fugiat repudiandae doloribus. Quidem accusamus necessitatibus accusantium, voluptatum totam
+                quos. Eligendi consequatur illo debitis! Iure delectus est recusandae dolore deleniti beatae accusamus
+                illo repudiandae voluptas! Doloremque sapiente animi, ullam esse sunt quaerat, quam consequatur
+                consectetur porro facilis blanditiis odio perspiciatis sequi, voluptatem hic minima tempora saepe
+                corporis labore. At nostrum veritatis nemo voluptate et illum tempora cum repellat nisi a est ipsam
+                temporibus animi, sunt dolore officia exercitationem maxime, veniam enim. Sit pariatur amet provident
+                nobis vel, cumque exercitationem cupiditate totam sequi rerum quo praesentium omnis dicta laborum ea,
+                explicabo dolores itaque quos sapiente. Reiciendis laudantium dolore sapiente numquam itaque dicta
+                magnam ad dolor perferendis repellat saepe rem, repudiandae voluptatem vitae cumque beatae mollitia
+                pariatur nam nisi! Corporis eum sunt eius vero dicta? Eveniet, qui? Dolorum libero animi obcaecati quod,
+                dolor officiis!</h3>
+        </div>
+    </center>
+</body>
+
+</html>
+
+<?php 
+include_once('http://localhost/ghotalanews/footer/footer.php');
+ ?>
